ec47bdf8d0567fec941d6e75a064ec6a4cabe14c
Taiwo Bukola <taiwobukola98@gmail.com>
2021-04-28 16:57:36 +0100

    new

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wmqdjb6c$zbif0rxg=^uv_d(!@%u#yv-1=9w60(b*!$whvp=7k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.hereokuapp.com','localhost','127.0.0.1']
 
